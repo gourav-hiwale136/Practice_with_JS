b2917df5f469f6107158e57fd3b3699570c52f8f
gourav-hiwale136 <gauravhiwale136@gmail.com>
2026-04-13 19:46:44 +0530

    Call Stack Examples

---
+++ core_js/script1.js
@@ -0,0 +1,19 @@
+
+
+
+//  Print the odd number in between 1 to 15 
+
+// 1st Method normal for loop
+// for (let i = 1; i <= 20; i = i += 2){
+//     console.log(i);
+    
+// }
+
+
+// 2nd method for odd number with for loop
+// for (let i = 1; i <= 20; i += 1){
+//     if (i % 2 === 1) {
+//         console.log(i);
+        
+//     }
+// }
