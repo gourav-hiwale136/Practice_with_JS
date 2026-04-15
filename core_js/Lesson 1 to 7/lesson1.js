df91430c19e66432b50ad5aa09c5ee92d18c16fa
gourav-hiwale136 <gauravhiwale136@gmail.com>
2026-04-15 20:55:30 +0530

    15/04/26

---
+++ core_js/Lesson 1 to 7/lesson1.js
@@ -0,0 +1,116 @@
+
+
+
+// let city = "Madurai"
+// console.log(city);
+
+
+// Addition
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 + num2);
+
+
+// let number = 7
+
+// if (number % 2 !== 0){
+//     console.log("Odd");
+    
+// }
+
+
+// Task 4
+
+// let age = 17
+// if (age >= 18) {
+//     console.log("Eligible to Vote");
+// } else {
+//     console.log("Not Eligible");
+    
+// }
+
+// Task 5
+
+// let a = 25,
+//      b = 18
+
+//      if (a > b) {
+//         console.log(a);
+//      } else if(b > a){
+//         console.log(b);
+//      }
+
+
+// Task 6
+
+// let number = 45
+// if (number % !5 === 0  ) {
+//     console.log(hi);
+    
+// } skip
+
+
+
+// Task 7
+// for (let i = 1; i <= 10; i++){
+    
+//     console.log(i);
+// }
+
+
+// Task 8 
+
+// for (let i = 20; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+// Task 9
+// let multiply = 1
+
+// for (let i = 5; i <= 50; i = i + 5){
+//     multiply = multiply * i
+// }
+// console.log(multiply);
+
+
+// Task 10 
+// let sum = 0;
+
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+// Task 11
+// for (let i = 1; i <= 30; i++){
+//     if (i % 3 === 0){
+//         console.log(i);
+//    }
+// }
+
+
+// Task 12
+// for (let i = 1; i <= 10; i ++){
+//     if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else {
+//         console.log(i);
+        
+//     }
+// }
+
+
+// Bonus 
+
+// let x = 2;
+
+// if(x > 1){
+//     x = x + 3;
+// }
+
+// console.log(x);
+
+
+// Output: 5
--- core_js/script1.js
+++ core_js/script1.js
@@ -1,19 +1,44 @@
 
 
-
-//  Print the odd number in between 1 to 15 
-
-// 1st Method normal for loop
-// for (let i = 1; i <= 20; i = i += 2){
+// Task 1
+// for (let i = 1; i <= 20; i++){
 //     console.log(i);
     
 // }
 
 
-// 2nd method for odd number with for loop
-for (let i = 1; i <= 20; i += 1){
-    if (i % 2 === 1) {
-        console.log(i);
+// Task 2
+// for (let i = 1; i <= 20; i = i + 1){
+//     if (i % 2 === 0) {
+//         console.log(i);
         
-    }
+//     }
+    
+// }
+
+
+// // Task 3
+// for (let i = 1; i <= 15; i++){
+//     if (i % 2 === 1) {
+//         console.log(i);
+//     }
+// }
+
+
+// Task 4
+// for (let i = 15; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+// Task 5
+let stars = ""
+
+for (let i = 1; i <= 5; i++){
+    stars = stars + "*"
+    console.log(stars);   
 }
+
+// Mini Challenge
+
+// output: 27
